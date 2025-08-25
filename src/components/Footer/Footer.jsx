7d7ac3801ae48b27885c64d9a59ab548ddf84bbd
Ryan B <bombingrun3@gmail.com>
2025-08-25 14:47:54 -0500

    Refactored Styling of Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,25 +3,24 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <div>
-        <ul className="footer__navbar">
-          <li className="footer__social">
-            <img className="footer__icon"></img>
-            <p className="footer__link">Instagram</p>
-          </li>
-          <li className="footer__social">
-            <img className="footer__icon"></img>
-            <p className="footer__link">Facebook</p>
-          </li>
-          <li className="footer__social">
-            <img className="footer__icon"></img>
-            <p className="footer__link">Venmo</p>
-          </li>
-        </ul>
-        <p className="footer__copyright">
-          All art is owned by Ben Shea - Ben Shea Studios - Ryan Bommarito
-        </p>
-      </div>
+      <ul className="footer__navbar">
+        <li className="footer__social">
+          <img className="footer__icon"></img>
+          <p className="footer__link">Instagram</p>
+        </li>
+        <li className="footer__social">
+          <img className="footer__icon"></img>
+          <p className="footer__link">Facebook</p>
+        </li>
+        <li className="footer__social">
+          <img className="footer__icon"></img>
+          <p className="footer__link">Venmo</p>
+        </li>
+      </ul>
+      <p className="footer__copyright">
+        All art is owned by Ben Shea - Ben Shea Studios - Developed by: Ryan
+        Bommarito
+      </p>
     </footer>
   );
 }
