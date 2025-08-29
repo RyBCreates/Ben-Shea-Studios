2770793860619168bfb40e92152caa487dc8bb4d
Ryan B <bombingrun3@gmail.com>
2025-08-28 20:07:50 -0500

    Refactored style for the hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,12 +3,14 @@ import "./Hero.css";
 function Hero() {
   return (
     <section className="hero">
-      <div>
+      <div className="hero__content">
         <div>
           <h2 className="hero__title">
-            Art is <span className="hero__title_bold">Cool.</span>
+            Experience Art in <span className="hero__title_style">Color.</span>
           </h2>
-          <p className="hero__subtitle">Buy this ART, because it's cool.</p>
+          <p className="hero__subtitle">
+            Original artwork available for purchase
+          </p>
         </div>
         <div className="hero__button-container">
           <button className="hero__button hero__button_main">
--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -3,7 +3,13 @@ import "./ItemCard.css";
 function ItemCard() {
   return (
     <div className="card">
-      <div className="card__content"></div>
+      <div className="card__content">
+        <img
+          className="card__image"
+          src="https://drive.google.com/uc?export=view&id=1lFyIh3qHN60iqa8AQyjClXPzyHj2CN3J"
+          alt="Artwork"
+        />
+      </div>
     </div>
   );
 }
