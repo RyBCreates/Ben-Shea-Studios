2595dd650c87fd6e14b0db93ea12d71b688c463b
Ryan B <bombingrun3@gmail.com>
2025-08-29 10:06:59 -0500

    Added a hamburger menu to the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,62 @@
+import { useState } from "react";
 import "./Header.css";
 
 function Header() {
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
+
+  const hamburgerToggle = () => {
+    setIsHamburgerOpen((prev) => !prev);
+  };
   return (
     <header className="header">
       <div className="header__logo">
-        <img className="header__icon"></img>
         <h1 className="header__title">Ben Shea Studios</h1>
       </div>
+      <button
+        type="button"
+        className="header__hamburger"
+        onClick={() => {
+          hamburgerToggle();
+        }}
+      >
+        |||
+      </button>
+
+      {isHamburgerOpen && (
+        <div className="header__hamburger-menu">
+          <button
+            type="button"
+            className="header__hamburger-option"
+            onClick={() => {
+              // navigate("favorite-recipes");
+              setIsHamburgerOpen(false);
+            }}
+          >
+            About
+          </button>
+          <button
+            type="button"
+            className="header__hamburger-option"
+            onClick={() => {
+              // navigate("shopping-list");
+              setIsHamburgerOpen(false);
+            }}
+          >
+            Exhibitions
+          </button>
+          <button
+            type="button"
+            className="header__hamburger-option"
+            onClick={() => {
+              // navigate("profile-settings");
+              setIsHamburgerOpen(false);
+            }}
+          >
+            Gallery
+          </button>
+        </div>
+      )}
+
       <ul className="navbar">
         <li className="navbar__link">About</li>
         <li className="navbar__link">Exhibits</li>
