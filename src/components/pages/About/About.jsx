a82e7ca4d1e61fb267c80773bd628de493e4c097
Ryan B <bombingrun3@gmail.com>
2025-09-01 16:29:59 -0500

    Added About section and put generic html

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -1,9 +1,24 @@
+import aboutBackground from "../../../assets/about-bg.png";
 import "./About.css";
 
 function About() {
   return (
     <section>
-      <h1>ABOUT SECTION</h1>
+      <img src={aboutBackground} />
+      <div className="about__summary">
+        <h1 className="about__title">About the Artist</h1>
+        <p className="about__summary-text">
+          Ben Shea is a renowned artist based in Boston, MA.
+        </p>
+      </div>
+      <div>
+        <h1>Biography</h1>
+        <p>
+          Ben makes art out of the passion of his heart. He started making his
+          art with vivid colors because he believes in inclusivity of all
+          colors.
+        </p>
+      </div>
     </section>
   );
 }
