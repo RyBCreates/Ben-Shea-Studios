d3539fe370547ae5add75b96dd61342cd517a0b5
Ryan B <bombingrun3@gmail.com>
2025-10-02 16:47:14 -0400

    Initial setup of orders tab

--- src/components/DashBoard/DashBoard.jsx
+++ src/components/DashBoard/DashBoard.jsx
@@ -1,4 +1,5 @@
 import SideBar from "../SideBar/SideBar";
+import Orders from "../Orders/Orders";
 import "./Dashboard.css";
 
 function DashBoard() {
@@ -8,7 +9,10 @@ function DashBoard() {
         <h2 className="admin__title admin__title_logged-in">
           Welcome to the Admin Dashboard
         </h2>
-        <SideBar />
+        <div className="dashboard__content">
+          <SideBar />
+          <Orders />
+        </div>
       </div>
     </section>
   );
---
+++ src/components/Orders/Orders.jsx
@@ -0,0 +1,24 @@
+import "./Orders.css";
+
+function Orders() {
+  return (
+    <div>
+      <h2 className="orders__title">Unfulfilled Orders</h2>
+      <ul>
+        <li>Here is an order</li>
+        <li>Here is an order</li>
+        <li>Here is an order</li>
+        <li>Here is an order</li>
+      </ul>
+      <h2 className="orders__title">Completed Orders</h2>
+      <ul>
+        <li>Here is an order</li>
+        <li>Here is an order</li>
+        <li>Here is an order</li>
+        <li>Here is an order</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Orders;
