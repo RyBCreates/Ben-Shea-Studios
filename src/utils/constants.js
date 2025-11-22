eee51a850abf5e8fae81aeb4dae55a707adcd640
Ryan B <bombingrun3@gmail.com>
2025-11-22 12:31:23 -0500

    Updated Base Url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://ben-shea-studio-backend.onrender.com";
+export const BASE_URL = "https://ben-shea-studio-backend.onrender.com/";
 
 //   export const BASE_URL ="http://localhost:4000";
